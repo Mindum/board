cfd2b7bf33fc589a25cda0395c496b808104568e
Mindum <thankq01@naver.com>
2024-01-10 16:31:18 +0900

    Entity

--- src/main/java/com/codingrecipe/board/entity/BoardEntity.java
+++ src/main/java/com/codingrecipe/board/entity/BoardEntity.java
@@ -1,9 +1,33 @@
 package com.codingrecipe.board.entity;
 
+import lombok.Getter;
+import lombok.Setter;
 
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "board_table")
 //db의 테이블 역할을 하는 클래스
 public class BoardEntity {
+    @Id // pk 컬럼 필수 지정
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
+    @Column(length = 20, nullable = false) // 크기는 20, not null
+    private String boardWriter;
 
+    @Column // 디폴트는 크기 255, null 가능
+    private String boardPass;
+
+    @Column
+    private String boardTitle;
+
+    @Column(length = 500)
+    private String boardContents;
+
+    @Column
+    private int boardHits;
 
 }
