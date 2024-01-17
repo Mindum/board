83d9be6848f09e8cd67c342a3a786e12e0e13031
Mindum <thankq01@naver.com>
2024-01-17 18:56:12 +0900

    기능구현

--- src/main/java/com/codingrecipe/board/dto/BoardDTO.java
+++ src/main/java/com/codingrecipe/board/dto/BoardDTO.java
@@ -1,6 +1,7 @@
 package com.codingrecipe.board.dto;
 
 
+import com.codingrecipe.board.entity.BoardEntity;
 import lombok.*;
 
 import java.time.LocalDateTime;
@@ -21,4 +22,18 @@ public class BoardDTO {
     private int boardHits;
     private LocalDateTime boardCreatedTime;
     private LocalDateTime boardUpdatedTime;
+
+
+    public static BoardDTO toBoardDTO(BoardEntity boardEntity){
+        BoardDTO boardDTO = new BoardDTO();
+        boardDTO.setId(boardEntity.getId());
+        boardDTO.setBoardWriter(boardEntity.getBoardWriter());
+        boardDTO.setBoardPass(boardEntity.getBoardPass());
+        boardDTO.setBoardTitle(boardEntity.getBoardTitle());
+        boardDTO.setBoardContents(boardEntity.getBoardContents());
+        boardDTO.setBoardHits(boardEntity.getBoardHits());
+        boardDTO.setBoardCreatedTime(boardEntity.getCreatedTime());
+        boardDTO.setBoardUpdatedTime(boardEntity.getUpdatedTime());
+        return boardDTO;
+    }
 }
--- src/main/java/com/codingrecipe/board/service/BoardService.java
+++ src/main/java/com/codingrecipe/board/service/BoardService.java
@@ -4,7 +4,16 @@ import com.codingrecipe.board.dto.BoardDTO;
 import com.codingrecipe.board.entity.BoardEntity;
 import com.codingrecipe.board.repository.BoardRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 
 // DTO -> Entity (엔티티 클래스에서 작업)
@@ -20,4 +29,52 @@ public class BoardService {
         boardRepository.save(boardEntity);
     }
 
+    public List<BoardDTO> findAll() {
+
+        List<BoardEntity> boardEntityList = boardRepository.findAll();
+        List<BoardDTO> boardDTOList = new ArrayList<>();
+        for(BoardEntity boardEntity: boardEntityList){
+            boardDTOList.add(BoardDTO.toBoardDTO(boardEntity));
+        }
+        return boardDTOList;
+
+    }
+
+    @Transactional //수동적인 쿼리 관리 할 때 사용댐
+    public void updateHits(Long id) {
+        boardRepository.updateHits(id);
+
+    }
+
+    public BoardDTO findById(Long id) {
+        Optional<BoardEntity> optionalBoardEntity = boardRepository.findById(id);
+        if(optionalBoardEntity.isPresent()){
+            BoardEntity boardEntity = optionalBoardEntity.get();
+            BoardDTO boardDTO = BoardDTO.toBoardDTO(boardEntity);
+            return boardDTO;
+        }else {
+            return null;
+        }
+    }
+
+    public BoardDTO update(BoardDTO boardDTO) {
+
+        BoardEntity boardEntity = BoardEntity.toUpdateEntity(boardDTO);
+        boardRepository.save(boardEntity);
+        return findById(boardDTO.getId());
+    }
+
+    public void delete(Long id) {
+        boardRepository.deleteById(id);
+    }
+
+    public Page<BoardDTO> paging(Pageable pageable) {
+        int page = pageable.getPageNumber() - 1;
+        int pageLimit = 3;
+        //한 페이지당 3개씩 글을 보여줌 id기준 내림차순 정렬
+        //page 위치에 있는 값은 0부터 시작
+        Page<BoardEntity> boardEntities =
+                boardRepository.findAll(PageRequest.of(page, pageLimit, Sort.by(Sort.Direction.DESC, "id")));
+        return boardEntities;
+    }
 }
